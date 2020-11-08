aee5b4e0aa912b3dc57cb24d4156433a14b2088f
Klotske <ilya@mokhov.vg>
2020-11-08 11:36:15 +0300

    Added solution code

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from PIL import Image
+
+import bitmap_helper
+
 
 t = np.linspace(0, 2 * np.pi)
 x = np.sin(t)
 y = np.cos(t)
 
 # Create & save plot
-fig = plt.figure(figsize=(6, 6))
+fig = plt.figure(figsize=(4, 4))
 plt.plot(x, y, 'k')
 plt.savefig('plot.png')
 
-# Load plot & convert to grayscale
-img = Image.open('plot.png')
-img = img.convert('1')
+# Load plot & rotate to fix image reading
+img = Image.open('plot.png').rotate(90)
+
+pixels = img.load()
+width, height = img.size
+print(width, height)
+
+all_pixels = []
+for x in range(width):
+    for y in range(height):
+        _pix = pixels[x, y]
+        mono_pix = 1 if round(sum(_pix)/float(len(_pix))) > 127 else 0
+        all_pixels.append(mono_pix)
+
+img.close()
+
+# Write bitmap
+file_name = 'plot_bitmap.bmp'
+bitmap_helper.create_monocrome_bitmap(file_name, all_pixels, width, height)
