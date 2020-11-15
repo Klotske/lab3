a42e9997cd45abffc40b8df8ec3ce0d6e52dbc98
Klotske <ilya@mokhov.vg>
2020-11-16 01:03:00 +0300

    Add code to create true monochrome bitmap

--- bitmap_helper.py
+++ bitmap_helper.py
@@ -1,4 +1,7 @@
+from math import ceil
+import re
 from struct import pack
+
 
 
 def create_header(width, height):
@@ -15,7 +18,7 @@ def create_info_header(width, height):
     image_width = width
     image_height = height
     planes = 1
-    bits_per_pixel = 8
+    bits_per_pixel = 1
     compression = 0
     image_size = 0
     x_pixels_per_meter = 0
@@ -36,9 +39,31 @@ def create_color_pallet():
     return pack('<8B', *color_0, *color_1)
 
 
-def create_pixel_data(pixels):
+def create_pixel_data(pixels, width):
+    bytes_in_row = ceil(width/32)*4
+    row_size = ceil(width/8)
     pixels.reverse()
-    pixel_data = pack(f'<{len(pixels)}B', *pixels)
+    pix_str = ''.join(map(str, pixels))
+
+    rows = re.findall('\\d{'+str(width)+'}', pix_str)
+    chunk_len = width if width < 8 else 8
+    res = []
+    for i in rows:
+        res.extend(re.findall('\\d{'+str(chunk_len)+'}', i))
+        if width % 8 != 0:
+            i_sub = re.sub('\\d{'+str(chunk_len)+'}', '', i)
+            res.append(i_sub)
+    res = [val.ljust(8, '0') for val in res]
+
+    row_str =  ''.join(res)
+    rows = re.findall('\\d{'+str(8*row_size)+'}', row_str)
+    rows = [val.ljust(8*bytes_in_row, '0') for val in rows]
+
+    row_str =  ''.join(rows)
+    rows = re.findall(r'\d{8}', row_str)
+    rows = [int(byte, base=2) for byte in rows]
+
+    pixel_data = pack(f'<{len(rows)}B', *rows)
     return pixel_data
 
 
@@ -47,14 +72,14 @@ def create_monochrome_bitmap(file_name, pixels, width, height):
         f.write(create_header(width, height))
         f.write(create_info_header(width, height))
         f.write(create_color_pallet())
-        f.write(create_pixel_data(pixels))
+        f.write(create_pixel_data(pixels, width))
 
 
 if __name__ == '__main__':
     width = 4
     height = 4
-    pix = [0, 1, 1, 0,
-           1, 0, 0, 1,
-           1, 1, 1, 1,
-           1, 0, 0, 1]
-    create_monochrome_bitmap('test.bmp', pix, 4, 4)
+    pix = [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+           1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+           1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    create_monochrome_bitmap('test.bmp', pix, 12, 4)
